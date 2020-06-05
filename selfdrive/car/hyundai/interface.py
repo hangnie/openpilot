c1c0423c7e2bf3e3b5fe5e515e0e8846f0f9271b
janpoo6427 <janpoo6427@gmail.com>
2020-06-05 21:02:19 +0900

    apply

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -431,6 +431,7 @@ class CarInterface(CarInterfaceBase):
     if ret.cruiseState.enabled and not self.cruise_enabled_prev:
       events.append(create_event('pcmEnable', [ET.ENABLE]))
     elif ret.cruiseState.enable and ret.gearShifter == GearShifter.drive and self.CS.clu_Vanz > 15:
+      print("vEgo:"+str(ret.vEgo) + "/vEgoRaw:" + str(ret.vEgoRaw))
       events.append(create_event('pcmEnable', [ET.ENABLE]))
     elif not ret.cruiseState.enabled:
       events.append(create_event('pcmDisable', [ET.USER_DISABLE]))
