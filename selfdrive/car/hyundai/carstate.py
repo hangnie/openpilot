f583ab193a96977ce7c57f92a69a15de29c33276
janpoo6427 <janpoo6427@gmail.com>
2020-06-18 23:01:32 +0900

    change type bool

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -69,7 +69,7 @@ class CarState(CarStateBase):
     ret.cruiseState.available = bool(cp.vl['EMS16']['CRUISE_LAMP_M'])
                                 #(cp_scc.vl["SCC11"]["MainMode_ACC"] != 0) if not self.no_radar else \
                                 #      cp.vl['EMS16']['CRUISE_LAMP_M'] != 0
-    ret.cruiseState.standstill = 4 #cp_scc.vl["SCC11"]['SCCInfoDisplay'] == 4. if not self.no_radar else False
+    ret.cruiseState.standstill = True #cp_scc.vl["SCC11"]['SCCInfoDisplay'] == 4. if not self.no_radar else False
     self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
     if ret.cruiseState.enabled:
       speed_conv = CV.MPH_TO_MS if self.is_set_speed_in_mph else CV.KPH_TO_MS
