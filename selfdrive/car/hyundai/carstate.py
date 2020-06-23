9b9d9b341a4e7a4d6048e3c0c5fb5aebad91f240
janpoo6427 <janpoo6427@gmail.com>
2020-06-23 20:02:29 +0900

    add clu vanz

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -63,6 +63,9 @@ class CarState(CarStateBase):
                                 #(cp_scc.vl["SCC11"]["MainMode_ACC"] != 0) if not self.no_radar else \
                                 #      cp.vl['EMS16']['CRUISE_LAMP_M'] != 0
     ret.cruiseState.standstill = cp_scc.vl["SCC11"]['SCCInfoDisplay'] == 4. if not self.no_radar else False
+
+    self.clu_Vanz = cp.vl["CLU11"]["CF_Clu_Vanz"]
+    
     self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
     if ret.cruiseState.enabled:
       speed_conv = CV.MPH_TO_MS if self.is_set_speed_in_mph else CV.KPH_TO_MS
